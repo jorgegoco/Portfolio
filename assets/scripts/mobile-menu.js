5d653e461748c19999364b4834e6ccee028bcec3
jorgegoco <jorgegoco70@gmail.com>
2022-10-11 13:42:48 +0200

    Change const to let in mobile-menu.js

--- assets/scripts/mobile-menu.js
+++ assets/scripts/mobile-menu.js
@@ -8,47 +8,47 @@ function hideMenu() {
   document.querySelector('#total-menu-mobile').style.display = 'none';
 }
 
-const section = document.createElement('div');
+let section = document.createElement('div');
 section.id = 'total-menu-mobile';
-const div = document.createElement('div');
+let div = document.createElement('div');
 div.id = 'menu-mobile';
-const img = document.createElement('img');
+let img = document.createElement('img');
 img.id = 'close-button';
 img.src = './assets/images/close-icon.svg';
 img.alt = 'close button';
 div.appendChild(img);
-const innerdiv = document.createElement('div');
+let innerdiv = document.createElement('div');
 innerdiv.className = 'contain-menu-link';
-const a1 = document.createElement('a');
+let a1 = document.createElement('a');
 a1.className = 'menu-link';
 a1.href = '#my-recent-works';
-const text1 = document.createTextNode('Portfolio');
+let text1 = document.createTextNode('Portfolio');
 a1.appendChild(text1);
 innerdiv.appendChild(a1);
-const a2 = document.createElement('a');
+let a2 = document.createElement('a');
 a2.className = 'menu-link';
 a2.href = '#about-me';
-const text2 = document.createTextNode('About');
+let text2 = document.createTextNode('About');
 a2.appendChild(text2);
 innerdiv.appendChild(a2);
-const a3 = document.createElement('a');
+let a3 = document.createElement('a');
 a3.className = 'menu-link';
 a3.href = '#contact-form';
-const text3 = document.createTextNode('Contact');
+let text3 = document.createTextNode('Contact');
 a3.appendChild(text3);
 innerdiv.appendChild(a3);
 div.appendChild(innerdiv);
 section.appendChild(div);
-const line = document.createElement('hr');
+let line = document.createElement('hr');
 line.className = 'menu-black-line';
 section.appendChild(line);
-const body = document.querySelector('body');
+let body = document.querySelector('body');
 body.appendChild(section);
 document.querySelector('#total-menu-mobile').style.display = 'none';
 
 document.querySelector('.mobile-menu').addEventListener('click', showMenu);
 document.querySelector('#close-button').addEventListener('click', hideMenu);
-const menuLinks = Array.from(document.querySelectorAll('.menu-link'));
+let menuLinks = Array.from(document.querySelectorAll('.menu-link'));
 for (let i = 0; i < menuLinks.length; i += 1) {
   menuLinks[i].addEventListener('click', hideMenu);
 }
