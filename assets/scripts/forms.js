21825f8dd444daa1e7732b5902829911db3e0d38
jorgegoco <jorgegoco70@gmail.com>
2022-10-13 19:14:36 +0200

    Fix linter errors

--- assets/scripts/forms.js
+++ assets/scripts/forms.js
@@ -2,24 +2,24 @@ const re = /[A-Z]/;
 const messageEmail = 'Email: Invalid characters. Capital letters not allowed';
 const form = document.querySelector('.form');
 
-form.addEventListener('submit', function(event) {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
-  let emailValidation = re.test(form.elements['email'].value);
-  if(!emailValidation) {
+  const emailValidation = re.test(form.elements.email.value);
+  if (!emailValidation) {
     form.submit();
-  } else if(form.lastElementChild.getAttribute('class') !== 'form-button') {
+  } else if (form.lastElementChild.getAttribute('class') !== 'form-button') {
     form.removeChild(form.lastElementChild);
-    let messageForm = document.createElement('p');
+    const messageForm = document.createElement('p');
     messageForm.className = 'invalid-email';
     messageForm.style.color = 'red';
-    let messageEmailText = document.createTextNode(messageEmail);
+    const messageEmailText = document.createTextNode(messageEmail);
     messageForm.appendChild(messageEmailText);
     form.appendChild(messageForm);
   } else {
-    let messageForm = document.createElement('p');
+    const messageForm = document.createElement('p');
     messageForm.className = 'invalid-email';
     messageForm.style.color = 'red';
-    let messageEmailText = document.createTextNode(messageEmail);
+    const messageEmailText = document.createTextNode(messageEmail);
     messageForm.appendChild(messageEmailText);
     form.appendChild(messageForm);
   }
