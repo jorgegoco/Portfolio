aedbb9e2efa5bac2bdf3beb2893abd3c5128f7bd
jorgegoco <jorgegoco70@gmail.com>
2022-10-15 18:13:23 +0200

    Fix eslint errors

--- assets/scripts/form-storage.js
+++ assets/scripts/form-storage.js
@@ -1,21 +1,21 @@
-document.addEventListener('load', (event) => {
-  let storedData = JSON.parse(localStorage.getItem('formData'));
+document.addEventListener('load', () => {
+  const storedData = JSON.parse(localStorage.getItem('formData'));
   document.querySelector('.form-name').value = storedData.name;
-  document.querySelector('.form-email').value = storedData['email'];
-  document.querySelector('.form-textarea').value = storedData['text'];
+  document.querySelector('.form-email').value = storedData.email;
+  document.querySelector('.form-textarea').value = storedData.text;
 });
 
-let submitForm = document.querySelector('.form');
-submitForm.addEventListener('submit', function() {
-  let formName = document.querySelector('.form-name').value;
-  let formEmail = document.querySelector('.form-email').value;
-  let formText = document.querySelector('.form-textarea').value;
+const submitForm = document.querySelector('.form');
+submitForm.addEventListener('submit', () => {
+  const formName = document.querySelector('.form-name').value;
+  const formEmail = document.querySelector('.form-email').value;
+  const formText = document.querySelector('.form-textarea').value;
 
-  let data = {
+  const data = {
     name: formName,
     email: formEmail,
-    text: formText
-  }
+    text: formText,
+  };
 
   localStorage.setItem('formData', JSON.stringify(data));
 });
