7e4d683fed380d6eb97167e810816f3af02502ea
jorgegoco <jorgegoco70@gmail.com>
2023-05-02 21:32:02 +0200

    Add animation to desktop header

--- assets/scripts/typewriter.js
+++ assets/scripts/typewriter.js
@@ -1,12 +1,23 @@
 const sentenceShort = '..with expertise in full-stack development using Ruby on Rails, JavaScript, React, and Redux. I collaborate with remote teams using GitHub, gitflow, and daily standups. I also mentor junior developers, improve code quality, and share motivation tips.';
+const sentenceLong = '..with expertise in full-stack development using Ruby on Rails, JavaScript, React, and Redux. I collaborate with remote teams using GitHub, gitflow, and daily standups. I also mentor junior developers, improve code quality, and share motivation tips. I am a lifelong learner and I am always looking for new challenges.';
 
 let i = 0;
 const speed = 40;
-function typeWriter() {
+function typeWriterMobile() {
   if (i < sentenceShort.length) {
     document.getElementById('typewriter-mobile').innerHTML += sentenceShort.charAt(i);
     i += 1;
-    setTimeout(typeWriter, speed);
+    setTimeout(typeWriterMobile, speed);
   }
 }
-typeWriter();
+typeWriterMobile();
+
+let j = 0;
+function typeWriterDesktop() {
+  if (j < sentenceLong.length) {
+    document.getElementById('typewriter-desktop').innerHTML += sentenceLong.charAt(j);
+    j += 1;
+    setTimeout(typeWriterDesktop, speed);
+  }
+}
+typeWriterDesktop();
